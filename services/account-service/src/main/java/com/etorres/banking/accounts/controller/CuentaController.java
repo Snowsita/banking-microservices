2292d861ab4f12cca07b7b028ccce815f9705e13
Snowsita <Snowsita@users.noreply.github.com>
2025-09-20 10:31:01 -0600

    feat: Added Controllers

--- services/account-service/src/main/java/com/etorres/banking/accounts/controller/CuentaController.java
+++ services/account-service/src/main/java/com/etorres/banking/accounts/controller/CuentaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/cuentas")
+@RequestMapping("/api/v1/accounts")
 public class CuentaController {
 
     private final CuentaService cuentaService;
--- services/account-service/src/main/java/com/etorres/banking/accounts/controller/MovimientoController.java
+++ services/account-service/src/main/java/com/etorres/banking/accounts/controller/MovimientoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/movimientos")
+@RequestMapping("/api/v1/movements")
 public class MovimientoController {
 
     private final MovimientoService movimientoService;
