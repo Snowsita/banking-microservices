0a325672a98aff08ee43ce2972845064c73404b3
Snowsita <Snowsita@users.noreply.github.com>
2025-09-19 20:35:13 -0600

    feat: Implemented Cuenta Service

--- services/account-service/src/main/java/com/etorres/banking/accounts/service/CuentaService.java
+++ services/account-service/src/main/java/com/etorres/banking/accounts/service/CuentaService.java
@@ -1,4 +1,15 @@
 package com.etorres.banking.accounts.service;
 
+import com.etorres.banking.accounts.dto.CuentaRequestDTO;
+import com.etorres.banking.accounts.dto.CuentaResponseDTO;
+
 public interface CuentaService {
+
+    /**
+     * Crea una nueva cuenta bancaria basada en los datos proporcionados en el DTO de solicitud.
+     *
+     * @param request El DTO que contiene los detalles necesarios para crear la cuenta.
+     * @return Un DTO de respuesta que contiene los detalles de la cuenta creada.
+     */
+    CuentaResponseDTO create(CuentaRequestDTO request);
 }
--- services/account-service/src/main/java/com/etorres/banking/accounts/service/CuentaServiceImpl.java
+++ services/account-service/src/main/java/com/etorres/banking/accounts/service/CuentaServiceImpl.java
@@ -1,4 +1,37 @@
 package com.etorres.banking.accounts.service;
 
-public class CuentaServiceImpl {
+import com.etorres.banking.accounts.dto.CuentaRequestDTO;
+import com.etorres.banking.accounts.dto.CuentaResponseDTO;
+import com.etorres.banking.accounts.model.Cuenta;
+import com.etorres.banking.accounts.repository.CuentaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CuentaServiceImpl implements CuentaService {
+
+    private final CuentaRepository cuentaRepository;
+
+    @Override
+    public CuentaResponseDTO create(CuentaRequestDTO request) {
+        Cuenta newCuenta = new Cuenta();
+        newCuenta.setAccountNumber(request.accountNumber());
+        newCuenta.setAccountType(request.accountType());
+        newCuenta.setInitialBalance(request.initialBalance());
+        newCuenta.setCurrentBalance(request.initialBalance());
+        newCuenta.setStatus(request.status());
+        newCuenta.setClientId(request.clientId());
+
+        Cuenta savedCuenta = cuentaRepository.save(newCuenta);
+
+        return new CuentaResponseDTO(
+                savedCuenta.getAccountNumber(),
+                savedCuenta.getAccountType(),
+                savedCuenta.getInitialBalance(),
+                savedCuenta.getCurrentBalance(),
+                savedCuenta.getStatus(),
+                savedCuenta.getClientId()
+        );
+    }
 }
