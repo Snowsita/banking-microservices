aafd18cceb509ac9d710c0d75218b41948bfb721
Snowsita <Snowsita@users.noreply.github.com>
2025-09-21 04:33:19 -0600

    fix: Typo at endpoint

--- services/client-service/src/main/java/com/etorres/banking/clients/controller/ClienteController.java
+++ services/client-service/src/main/java/com/etorres/banking/clients/controller/ClienteController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/clientes")
+@RequestMapping("/api/v1/clients")
 public class ClienteController {
 
     private final ClienteService clienteService;
